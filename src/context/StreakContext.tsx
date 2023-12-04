b34c5a67dd241eca307c2819dba0cb4873e84224
petarkosic <avt3914@gmail.com>
2023-12-04 23:38:47 +0100

    add: streak context last updated timestamp
    Set last updated timestamp to local storage to make sure that the streak doesn't update more than once every day.

--- src/context/StreakContext.tsx
+++ src/context/StreakContext.tsx
@@ -3,34 +3,60 @@ import { createContext, useContext, useState, useEffect } from 'react';
 type TStreak = {
 	dailyStreak: number;
 	updateStreak: () => void;
+	lastUpdatedTimestamp: number | null;
 };
 
 const StreakContext = createContext<TStreak>({
 	dailyStreak: 1,
 	updateStreak: () => {},
+	lastUpdatedTimestamp: null,
 });
 
 export const useStreak = () => useContext(StreakContext);
 
 export const StreakProvider = ({ children }: { children: React.ReactNode }) => {
 	const [dailyStreak, setDailyStreak] = useState(1);
+	const [lastUpdatedTimestamp, setLastUpdatedTimestamp] = useState<
+		number | null
+	>(null);
 
 	useEffect(() => {
 		const dailyStreakFromLocalStorage = localStorage.getItem('dailyStreak');
+		const lastUpdatedTimestampFromLocalStorage = localStorage.getItem(
+			'lastUpdatedTimestamp'
+		);
 
 		if (dailyStreakFromLocalStorage) {
 			setDailyStreak(parseInt(dailyStreakFromLocalStorage));
 		}
+
+		if (lastUpdatedTimestampFromLocalStorage) {
+			setLastUpdatedTimestamp(parseInt(lastUpdatedTimestampFromLocalStorage));
+		}
 	}, [dailyStreak]);
 
 	const updateStreak = () => {
-		// TODO: update the streak
-		setDailyStreak((prevStreak) => prevStreak + 1);
-		localStorage.setItem('dailyStreak', String(dailyStreak + 1));
+		const currentTimeStamp = new Date().getTime();
+
+		// check whether the current time is more than 24 hours (24 * 60 * 60 * 1000 milliseconsds) from the last updated timestamp
+		if (
+			!lastUpdatedTimestamp ||
+			currentTimeStamp - lastUpdatedTimestamp > 24 * 60 * 60 * 1000
+		) {
+			setLastUpdatedTimestamp(currentTimeStamp);
+			localStorage.setItem('lastUpdatedTimestamp', String(currentTimeStamp));
+
+			setDailyStreak((prevStreak) => prevStreak + 1);
+			localStorage.setItem('dailyStreak', String(dailyStreak + 1));
+		} else {
+			console.log('Streak already updated today');
+		}
 	};
 
 	return (
-		<StreakContext.Provider value={{ dailyStreak, updateStreak }}>
+		<StreakContext.Provider
+			value={{ dailyStreak, updateStreak, lastUpdatedTimestamp }}
+		>
 			{children}
 		</StreakContext.Provider>
 	);
