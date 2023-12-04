5a189d6ba7b1e693bb01a5c7f12014ef1da3952d
petarkosic <avt3914@gmail.com>
2023-12-04 23:49:36 +0100

    add: working progress bar
    Update progress bar based on daily goal.

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,17 +1,34 @@
+import { useRef } from 'react';
+import { useDailyGoal } from '../context/DailyGoalContext';
 import { useStreak } from '../context/StreakContext';
 import {
 	CircularProgressbarWithChildren,
 	buildStyles,
 } from 'react-circular-progressbar';
+import { useDailyWordsCounter } from '../context/DailyWordsCounterContext';
+import { getWordCountForGoal } from '../utils/getWordCountForGoal';
 
 const ProgressBar = () => {
 	const { dailyStreak } = useStreak();
+	const { dailyGoal } = useDailyGoal();
+
+	const { dailyWordsCounter } = useDailyWordsCounter();
+
+	const numOfWordsForDailyStreak = useRef<number>();
+
+	if (dailyGoal == 'casual') {
+		numOfWordsForDailyStreak.current = getWordCountForGoal('casual');
+	} else if (dailyGoal == 'regular') {
+		numOfWordsForDailyStreak.current = getWordCountForGoal('regular');
+	} else if (dailyGoal == 'serious') {
+		numOfWordsForDailyStreak.current = getWordCountForGoal('serious');
+	}
 
 	return (
 		<div style={{ width: '50px' }}>
 			<CircularProgressbarWithChildren
-				value={4}
-				maxValue={10}
+				value={dailyWordsCounter}
+				maxValue={numOfWordsForDailyStreak.current}
 				styles={buildStyles({
 					textColor: '#000',
 					pathColor: '#00ff00',
